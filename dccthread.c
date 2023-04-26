0d42783d5fd35bfa069a669746ef541877a527a5
Daniel Neiva <neiva.daniel@outlook.com>
2023-04-26 01:54:37 -0300

    tests 1 - 4 passing

--- dccthread.c
+++ dccthread.c
@@ -9,6 +9,7 @@
 
 ucontext_t manager;
 struct dlist *thread_list;
+struct dlist *done_thread_list;
 int currentIndex = 0;
 
 // typedef struct schedule {
@@ -23,16 +24,14 @@ struct dccthread {
     char stack[STACKSIZE];
     void (*func)(int);
     int param;
-    int isRunning;
 };
 
 void schedule(void) {
-    while(1) {
-        dccthread_t *current = dccthread_self();
-        dccthread_yield();
-        dlist_push_right(thread_list, current);
-        dccthread_t *next = dlist_pop_left(thread_list);
+    dccthread_t *next;
+    while(thread_list->head != NULL) {
+        next = dlist_pop_left(thread_list);
         swapcontext(&manager, &next->context);
+        dlist_push_right(done_thread_list, next);
     }
 }
 
@@ -47,7 +46,8 @@ void dccthread_create_manager(void) {
 void dccthread_init(void (*func)(int), int param) {
     dccthread_create_manager();
     thread_list = dlist_create();
-    dccthread_t *main = dccthread_create("main", func, param);
+    done_thread_list = dlist_create();
+    dccthread_create("main", func, param);
     setcontext(&manager);
 }
 
@@ -58,28 +58,20 @@ dccthread_t *dccthread_create(const char *name, void (*func)(int), int param) {
     thread->context.uc_stack.ss_sp = thread->stack;
     thread->context.uc_stack.ss_size = sizeof(thread->stack);
     thread->context.uc_link = &manager;
-    thread->isRunning = 0;
     makecontext(&thread->context, (void (*)(void))func, 1, param); 
     dlist_push_right(thread_list, thread);
     return thread;
 }
 
 void dccthread_yield(void) {
-    dccthread_t *current = dccthread_self();
+    dccthread_t *current = dlist_pop_left(thread_list);
     if (current != NULL) {
-        current->isRunning = 0;
+        dlist_push_right(done_thread_list, current);
         swapcontext(&current->context, &manager);
     }
 }
 
 dccthread_t *dccthread_self(void) {
-    struct dnode *aux = thread_list->head;
-    if (aux == NULL || aux->data == NULL) return NULL;
-    dccthread_t *data = (dccthread_t *) aux->data;
-    if (thread_list->count == 1 && data->isRunning == 0) return NULL;
-    while(data->isRunning != 1 && aux->next != NULL) {
-        aux = aux->next;
-        data = (dccthread_t *) aux->data;
-    }
-    return aux->data;
+    struct dnode *head = thread_list->head;
+    return head == NULL ? NULL : (dccthread_t *) head->data;
 }
