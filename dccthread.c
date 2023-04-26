a2e3450a84dbc3148c6701d91c55fa0fb66bed1e
Daniel Neiva <neiva.daniel@outlook.com>
2023-04-26 00:25:15 -0300

    wip: scheduling

--- dccthread.c
+++ dccthread.c
@@ -7,22 +7,46 @@
 
 #define STACKSIZE 32768
 
-static ucontext_t manager, context1, context2;
-struct dlist thread_list;
+ucontext_t manager;
+struct dlist *thread_list;
+int currentIndex = 0;
 
-//#include "list.h"
+// typedef struct schedule {
+//     ucontext_t context;
+//     char stack[STACKSIZE];
+//     int running;
+//     dccthread_t **threads;
+// } schedule_t;
 struct dccthread {
     ucontext_t context;
     const char *name;
     char stack[STACKSIZE];
     void (*func)(int);
     int param;
+    int isRunning;
 };
 
+void schedule(void) {
+    dccthread_t *current = dccthread_self();
+    //dccthread_yield();
+    dlist_push_right(thread_list, current);
+    dccthread_t *next = dlist_pop_left(thread_list);
+    swapcontext(&manager, &next->context);
+}
+
+void dccthread_create_manager(void) {
+    getcontext(&manager);
+    manager.uc_stack.ss_sp = malloc(STACKSIZE);
+    manager.uc_stack.ss_size = STACKSIZE;
+    manager.uc_link = NULL;
+    makecontext(&manager, schedule, 0);
+}
 
 void dccthread_init(void (*func)(int), int param) {
+    dccthread_create_manager();
+    thread_list = dlist_create();
     dccthread_t *main = dccthread_create("main", func, param);
-    swapcontext(&manager, &main->context);
+    setcontext(&manager);
 }
 
 dccthread_t *dccthread_create(const char *name, void (*func)(int), int param) {
@@ -32,10 +56,24 @@ dccthread_t *dccthread_create(const char *name, void (*func)(int), int param) {
     thread->context.uc_stack.ss_sp = thread->stack;
     thread->context.uc_stack.ss_size = sizeof(thread->stack);
     thread->context.uc_link = &manager;
+    thread->isRunning = 0;
     makecontext(&thread->context, (void (*)(void))func, 1, param); 
-    dlist_push_right(&thread_list, thread);
+    dlist_push_right(thread_list, thread);
     return thread;
 }
 
 void dccthread_yield(void) {
+    dccthread_t *current = dccthread_self();
+    current->isRunning = 0;
+    swapcontext(&current->context, &manager);
+}
+
+dccthread_t *dccthread_self(void) {
+    struct dnode *aux = thread_list->head;
+    dccthread_t *data = (dccthread_t *) aux->data;
+    while(data->isRunning != 1 && aux->next != NULL) {
+        aux = aux->next;
+        data = (dccthread_t *) aux->data;
+    }
+    return aux->data;
 }
